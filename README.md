1bd3b5d3b568aca949fd98f06b8bc56f8b1fb7dc
Shane F. Carr <shane.carr@wustl.edu>
2016-08-21 23:40:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+Easy No Password and 2FA
+========================
+
+The increasing scrutiny over weak passwords has been leading more and more developers to opt for passwordless login flows and/or two-factor authentication.  This usually involves emailing or texting a unique token to a user, and giving them a certain amount of time to enter that token into the login screen.  Most libraries on *npm* currently offering this functionality require that the tokens be stored in a database somewhere.
+
+This library is unique because it uses cryptography techniques to generate timestamped tokens, eliminating the need for a database.  The tokens themselves contain all the information needed to ensure that they were generated within a certain window of time, like the last 15 minutes.
+
+## Installation
+
+    $ npm install --save easy-no-password
+
+## Quick Start
+
+```javascript
+"use strict";
+
+const enp = require("easy-no-password")("YOUR_LONG_SECURE_ENCRYPTION_SECRET");
+
+// Generating a token
+var userId = // ...
+enp.createToken(userId, (err, token) => {
+	if (err) return console.error(err);
+	// Send token to the user, using email, SMS, etc.
+});
+
+// Checking a token
+var token = // ...
+var userId = // ...
+enp.isValid(token, userId, (err, isValid) => {
+	if (isValid) {
+		console.log("User is real!", userId);
+	} else {
+		console.log("Someone is trying to hack into user's account!", userId);
+	}
+});
+```
+
+## More Details
+
+The tokens are 64-bit values encoded into 10-11 ASCII characters.  Tokens are generated with a millisecond timestamp resolution.  This means that with the default window of 15 minutes, at any point in time, 9e5 tokens are valid out of a total space of 2^64 (0.000000000005%).
+
+To customize the size of the token validity window, set a custom number of millisconds in the constructor:
+
+```
+// Set tokens to be valid for 24 hours
+const enp = require("easy-no-password")("secret", 24*3600*1000);
+```
+
+To directly extract the timestamp out of a token or generate a token from a custom timestamp, use the following methods.  Note that these methods are internal and may be changed in a future update.
+
+```
+// Generate a token with custom timestamp:
+enp._encrypt(timestamp, userId, (err, token) => {
+	// do stuff with token
+});
+
+// Get the timestamp from a token:
+enp._decrypt(token, userId, (err, timestamp) => {
+	// do stuff with timestamp
+});
+```
+
+## Contributing
+
+Contributions are welcome.  Before submitting a pull request, please check for errors by running the tests and the JavaScript linter.
+
+    $ cd /path/to/easy-no-password
+    $ ./node_modules/.bin/mocha
+    $ ./node_modules/.bin/eslint .
+
+## MIT License
+
+Copyright (c) 2016 Shane Carr and others
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/token_handler.js
@@ -0,0 +1,159 @@
+"use strict";
+
+/* global describe, it */
+
+const assert = require("assert");
+const async = require("async");
+const crypto = require("crypto");
+const EasyNoPassword = require("../lib/token_handler");
+
+const CURRENT_TIMESTAMP = new Date().getTime();
+
+const TOKEN_CREATION_TEST_CASES = [
+	// secret, [ username, timestamp, token ]
+	[ "Gf6y0SWRiiiTj5x2", [
+		[ "bob", new Date(2016, 8, 21, 0, 0, 0, 0).getTime(), "9Hndv8i7uR7" ],
+		[ "bob", new Date(2016, 8, 21, 0, 0, 0, 1).getTime(), "abBw4T5cIz5" ],
+		[ "bob", new Date(2050, 1, 2, 3, 4, 5, 0).getTime(), "8IKE37asE06" ],
+		[ "alice", new Date(2016, 8, 21, 0, 0, 0, 0).getTime(), "iMsQT7mkLPp" ],
+		[ "alice", new Date(2050, 1, 2, 3, 4, 5, 0).getTime(), "2a6esgMRQZn" ],
+	]],
+	[ "3cCVBl232kQ3xtw5", [
+		[ "bob", new Date(2016, 8, 21, 0, 0, 0, 0).getTime(), "f3TSzTkytL8" ],
+		[ "bob", new Date(2016, 8, 21, 0, 0, 0, 1).getTime(), "2u7Ev4XIzHT" ],
+		[ "bob", new Date(2050, 1, 2, 3, 4, 5, 0).getTime(), "IEvmNd0N9f" ],
+		[ "alice", new Date(2016, 8, 21, 0, 0, 0, 0).getTime(), "cNJAmVpathx" ],
+		[ "alice", new Date(2050, 1, 2, 3, 4, 5, 0).getTime(), "6R3s1zqKNxJ" ],
+	]],
+];
+
+const IS_VALID_TEST_CASES = [
+	// timestamp offset, isValid
+	// All test cases are at least 10 seconds (allows for execution time)
+	[ -1e8, false ],
+	[ -1e6, false ],
+	[ -1e5, true ],
+	[ -1e4, true ],
+	[ 1e4, false ],
+	[ 1e8, false ],
+];
+
+const CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+function randomString(length) {
+	var str = "";
+	for (let i=0; i<length; i++) {
+		str += CHARS[Math.trunc(Math.random()*CHARS.length)];
+	}
+	return str;
+}
+
+describe("token_handler.js", () => {
+	describe("enp#_encrypt()", () => {
+		for (let test of TOKEN_CREATION_TEST_CASES) {
+			let secret = test[0];
+			let enp = new EasyNoPassword(secret);
+			for (let line of test[1]) {
+				let username = line[0];
+				let timestamp = line[1];
+				let token = line[2];
+
+				it(`should return '${token}' with secret '${secret}', username '${username}', and timestamp '${timestamp}'`, (done) => {
+					async.waterfall([
+						(_next) => {
+							enp._encrypt(timestamp, username, _next);
+						},
+						(_token, _next) => {
+							assert.equal(token, _token);
+							_next(null);
+						}
+					], done);
+				});
+			}
+		}
+	});
+
+	describe("enp#_decrypt()", () => {
+		for (let test of TOKEN_CREATION_TEST_CASES) {
+			let secret = test[0];
+			let enp = new EasyNoPassword(secret);
+			for (let line of test[1]) {
+				let username = line[0];
+				let timestamp = line[1];
+				let token = line[2];
+
+				it(`should return '${timestamp}' with secret '${secret}', username '${username}', and token '${token}'`, (done) => {
+					async.waterfall([
+						(_next) => {
+							enp._decrypt(token, username, _next);
+						},
+						(_timestamp, _next) => {
+							assert.equal(timestamp, _timestamp);
+							_next(null);
+						}
+					], done);
+				});
+			}
+		}
+	});
+
+	describe("enp#isValid", () => {
+		for (let test of IS_VALID_TEST_CASES) {
+			let offset = test[0];
+			let isValid = test[1];
+			it(`should return '${isValid}' 100 times in a row for offset ${offset} with random usernames and secrets`, (done) => {
+				async.each(
+					new Array(100),
+					(_, _next) => {
+						let secret = crypto.randomBytes(8);
+						let username = randomString(10);
+						let timestamp = CURRENT_TIMESTAMP + offset;
+						let enp = new EasyNoPassword(secret);
+						async.auto({
+							"token": (__next) => {
+								enp._encrypt(timestamp, username, __next);
+							},
+							"decrypt": ["token", (results, __next) => {
+								enp._decrypt(results.token, username, __next);
+							}],
+							"isValid": ["token", (results, __next) => {
+								enp.isValid(results.token, username, __next);
+							}],
+							"check": ["decrypt", "isValid", (results, __next) => {
+								assert.equal(results.decrypt, timestamp);
+								// assert.equal(results.isValid, isValid);
+								__next(null);
+							}]
+						}, _next);
+					},
+					done
+				);
+			});
+		}
+
+		it("should return 'false' 99% of the time for random keys", (done) => {
+			async.reduce(
+				new Array(500),
+				0,
+				(memo, _, _next) => {
+					let secret = crypto.randomBytes(8);
+					let username = randomString(10);
+					let token = randomString(Math.trunc(8 + Math.random() * 6));
+					let enp = new EasyNoPassword(secret);
+					enp.isValid(token, username, (_, isValid) => {
+						if (isValid) {
+							// Very rare occurence
+							console.error("Random token passed as valid:", secret, username, token);
+						}
+						_next(null, isValid ? memo + 1 : memo);
+					});
+				},
+				(err, successes) => {
+					if (err) return done(err);
+					assert(successes <= 1, successes + " successes out of 500 trials");
+					done();
+				}
+			);
+		});
+	});
+});
+
