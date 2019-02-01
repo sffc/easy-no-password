50a5366fae2113e0ec4f7c1e93bcd352ae4b5033
Shane F. Carr <shane.carr@wustl.edu>
2019-01-31 21:43:49 -0700

    Point index.js to lib

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var EasyNoPassword = require("./build/token_handler");
-var EasyStrategy = require("./build/strategy");
+var EasyNoPassword = require("./lib/token_handler");
+var EasyStrategy = require("./lib/strategy");
 
 function construct(secret, maxTokenAge) {
 	return new EasyNoPassword(secret, maxTokenAge);
