d1b8591e3e407a6a1d24c3d4de8d1bc29b6daf59
frani1 <franinnocenti@gmail.com>
2020-10-15 14:24:36 -0300

    add async await module

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var EasyNoPassword = require("./lib/token_handler");
-var EasyStrategy = require("./lib/strategy");
+let EasyNoPassword = require('./lib/token_handler')
+let enp = require('./lib/handler')
+let EasyStrategy = require('./lib/strategy')
 
 function construct(secret, maxTokenAge) {
-	return new EasyNoPassword(secret, maxTokenAge);
+  return new EasyNoPassword(secret, maxTokenAge)
 }
-construct.Strategy = EasyStrategy;
+construct.Strategy = EasyStrategy
+construct.AsyncAwait = enp
 
-module.exports = construct;
+module.exports = construct
