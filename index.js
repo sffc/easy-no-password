daf4766e861add54ab2df2c21b85042f781abd41
frani1 <franinnocenti@gmail.com>
2020-11-11 22:20:03 -0300

    setup only async/await

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-let EasyNoPassword = require("./lib/token_handler");
-let enp = require("./lib/handler");
-let EasyStrategy = require("./lib/strategy");
+let enp = require('./lib/handler')
 
-function construct(secret, maxTokenAge) {
-	return new EasyNoPassword(secret, maxTokenAge);
-}
-construct.Strategy = EasyStrategy;
-construct.AsyncAwait = enp;
-
-module.exports = construct;
+module.exports = enp
